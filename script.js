6d0e2951e2b00bcf9ff23daaaf8721b50a579057
RoddyTwoHotty <sirodkhan0040@afscholars.org>
2025-05-22 13:13:33 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+import React from "https://esm.sh/react@19";
+import ReactDOM from "https://esm.sh/react-dom@19/client";
+
+
+function App() {
+  return (
+        <div className= "app">
+      <h1 className= "title" >Trivia!</h1>
+    </div>
+  );
+}
+
+//This code below will allow the App Component to render(show up on the webview)!
+/*const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<App />);
+*/
